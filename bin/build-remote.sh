e9598281b6e54d41537da6566964be331ee53c54
Jon Goodger <jonno.is@gmail.com>
2024-10-28 00:24:48 +0000

    Add remote building with CodeBuild

--- bin/build-remote.sh
+++ bin/build-remote.sh
@@ -2,7 +2,7 @@
 
 PROJECT_NAME=$(${TERRAFORM_COMMAND:-terraform} output -raw codebuild_project_name)
 
-AWS_PAGER= ${AWS_COMMAND:-aws} codebuild start-build --project-name=$PROJECT_NAME
+AWS_PAGER= ${AWS_COMMAND:-aws} codebuild start-build-batch --project-name=$PROJECT_NAME
 
 echo "Tailing build logs, nothing will show until source download completes (ctrl-c to cancel)..."
 
